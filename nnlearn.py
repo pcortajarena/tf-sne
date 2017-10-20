81a93e5242bbd4eca75cfd0a148946af1c36b093
pcortajarena <p.cortajarena@alumnos.upm.es>
2017-10-20 15:55:29 +0000

    bugfix: errors & plot

--- nnlearn.py
+++ nnlearn.py
@@ -5,6 +5,8 @@ from keras.wrappers.scikit_learn import KerasRegressor
 from sklearn.datasets import load_boston
 from sklearn.model_selection import KFold
 import keras.backend as K
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.preprocessing import StandardScaler
@@ -14,6 +16,7 @@ from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 from sklearn.manifold import MDS
 import pandas as pd
+import numba
 
 
 #load data
@@ -23,43 +26,44 @@ def load_dataset(csv_path):
 
 #distance loss
 def dist_loss(y, y_):
-    y, y_ = K.flatten(y), K.flatten(y_)
-    return K.sqrt(K.sum(K.square(y - y_)))
-
-
-def mean_loss(y, y_):
-    y, y_ = K.flatten(y), K.flatten(y_)
-    return K.mean(K.square(y - y_))
+    # y, y_ = K.flatten(y), K.flatten(y_)
+    return K.mean(K.sqrt(K.sum(K.square(y - y_), axis=1)))
 
 
 def dist_error(y_true, y_pred):
-    return np.sqrt(np.sum((y_true - y_pred) ** 2))
+    return np.mean(np.sqrt(np.sum((y_true - y_pred) ** 2, axis=1)))
 
 
-def mean_error(y_true, y_pred):
-    return np.mean((y_true - y_pred) ** 2)
+# def mean_error(y_true, y_pred):
+#     return np.mean((y_true - y_pred) ** 2)
+
+
+# def mean_loss(y, y_):
+#     y, y_ = K.flatten(y), K.flatten(y_)
+#     return K.mean(K.square(y - y_))
+
 
 
 #neural net model
 def reg(input_shape, loss_func, output_shape, act_function='tanh'):
     model = Sequential()
 
-    model.add(Dense(10, input_shape=(input_shape,)))
+    model.add(Dense(20, input_shape=(input_shape,)))
     model.add(Activation(act_function))
-    # model.add(Dropout(0.8))
+    model.add(Dropout(0.2))
 
-    # model.add(Dense(5, input_shape=(input_shape,)))
-    # model.add(Activation(act_function))
-    # model.add(Dropout(0.8))
+    model.add(Dense(10))
+    model.add(Activation(act_function))
+    model.add(Dropout(0.25))
 
-    # model.add(Dense(20, input_shape=(input_shape,)))
-    # model.add(Activation(act_function))
-    # model.add(Dropout(0.8))
+    model.add(Dense(5))
+    model.add(Activation(act_function))
+    model.add(Dropout(0.3))
 
     model.add(Dense(output_shape))
     model.add(Activation(act_function))
 
-    ada = optimizers.Adagrad(lr=0.01)
+    ada = optimizers.Adagrad(lr=1)
 
     model.compile(optimizer=ada, loss=loss_func)
     return model
@@ -74,6 +78,12 @@ def sctrans(X, trans):
     X_ = trans.fit_transform(X_scaled)
 
     return X_scaled, X_
+
+
+def plot_similarity(y_true, y_pred):
+    plt.scatter(y_true[:,0], y_true[:,1])
+    plt.scatter(y_pred[:,0], y_pred[:,1], alpha=0.5)
+    plt.savefig('similarity.png')
 
 
 #cv
@@ -118,8 +128,8 @@ if __name__ == '__main__':
     #fold dataset
     kf = KFold(n_splits=3, shuffle=True)
     error_kfold, cv_preds = cv_function(model, kf, X_scaled, X_, dist_error)
+    print(error_kfold)
 
     #plot
-    plt.scatter(X_[:,0], X_[:,1])
-    plt.scatter(cv_preds[:,0], cv_preds[:,1], alpha=0.5)
-    plt.show()
+    plot_similarity(X_, cv_preds)
+
