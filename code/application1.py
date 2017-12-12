9e923970c4932d36222043e135e425868bc4b573
pcortajarena <p.cortajarena@alumnos.upm.es>
2017-12-12 16:30:14 +0000

    application 1: dictionary of models and minimum error values

--- code/application1.py
+++ code/application1.py
@@ -1,3 +1,5 @@
+import matplotlib
+matplotlib.use("Agg")
 import pandas as pd
 import numpy as np
 
@@ -25,6 +27,13 @@ def dist_loss(y, y_):
 def dist_error(y_true, y_pred):
     return np.mean(np.sqrt(np.sum((y_true - y_pred) ** 2, axis=1)))
 
+#percentage error
+def percentage_error(err, X_div):
+    maximo = X_div.max(axis=0)
+    minimo = X_div.min(axis=0)
+    rango = maximo - minimo
+    return err / rango
+
 #neural net model
 def reg(shape, loss_func, layers, dropout, output_shape, lr, act_function='tanh'):
 
@@ -44,18 +53,49 @@ def reg(shape, loss_func, layers, dropout, output_shape, lr, act_function='tanh'
     model.compile(optimizer=ada, loss=loss_func)
     return model
 
-def app(unsuper_pipe, super_pipe, X, y):
+#neural net replicating
+def app(X_unsupervised, dic, X, y):
 
-    X_unsupervised = unsuper_pipe.fit_transform(X)
+    resultados = {key: [] for key in dic}
 
-    X_train, X_test, X_unsuper_train, X_unsuper_test, y_train, y_test = train_test_split(X, X_unsupervised, y)
+    X_train, X_test, X_unsuper_train, X_unsuper_test, y_train, y_test = train_test_split(
+        X, X_unsupervised, y, random_state=0)
 
-    super_pipe.fit(X_train, X_unsuper_train)
+    min_error = 1000
 
-    predictions = super_pipe.predict(X_test)
+    for model_name, model_params in dic.items():
+        nn = lambda: reg(
+            X.shape[1], loss_func=dist_loss,
+            layers=model_params['layers'], dropout=model_params['dropout'],
+            output_shape=2, lr=0.1, act_function='sigmoid')
 
-    return predictions, X_unsuper_test, y_test
+        neuralmodel = KerasRegressor(nn, epochs=200, batch_size=10)
+        
+        super_pipe = make_pipeline(
+            StandardScaler(),
+            neuralmodel)
 
+        super_pipe.fit(X_train, X_unsuper_train)
+
+        predictions = super_pipe.predict(X_test)
+
+        error = dist_error(X_unsuper_test[:,:2], predictions[:,:2])
+
+        if min_error > error:
+            min_error = error
+            result_predic = predictions
+            result_unsuper_test = X_unsuper_test
+            result_y = y_test
+
+        perc = percentage_error(error, X_unsuper_test)
+
+        res = [error]
+        for e in perc:
+            res.append(e)
+
+        resultados[model_name] += res
+
+    return resultados, result_predic, result_unsuper_test, result_y
 
 if __name__ == '__main__':
     
@@ -71,28 +111,39 @@ if __name__ == '__main__':
         tsne
     )
 
-    nn = lambda: reg(
-        X.shape[1], loss_func=dist_loss,
-        layers=[1000], dropout=[0],
-        output_shape=2, lr=0.1, act_function='sigmoid')
+    X_unsup = unsupervised_pipeline.fit_transform(X)
 
-    model = KerasRegressor(nn, epochs=200, batch_size=10)
-
-    supervised_pipeline = make_pipeline(
-        StandardScaler(),
-        model
-    )
-
-    total_predictions, X_tsne_test, y_test = app(unsupervised_pipeline, supervised_pipeline, X, y)
+    #iterate models
+    models = {
+        "model1": {
+            "layers": [32,16,8],
+            "dropout": [0,0,0]
+        },
+        "model2":{
+            "layers": [32,16,8],
+            "dropout": [0,0,0]
+        },
+        "model3":{
+            "layers": [128,64,64],
+            "dropout": [0,0,0]
+        },
+        "model4": {
+            "layers": [128,64],
+            "dropout": [0,0]
+        },
+        "model5":{
+            "layers": [1000],
+            "dropout": [0]
+        }
+    }
+    
+    results = app(X_unsup, models, X, y)
 
     #example of number representation
     fig, ax = plt.subplots(1,1, figsize=(9,8))
     heatmap(X[550].reshape(8,8), cmap='gray', annot=True, ax=ax, cbar=False)
     plt.axis('off')
     plt.savefig('../text/figures/exampledigit.pdf', bbox_inches='tight')
-
-    error = dist_error(X_tsne_test.iloc[:,:2].values, total_predictions.iloc[:,:2].values)
-
 
 
 
